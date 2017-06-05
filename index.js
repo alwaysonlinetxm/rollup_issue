7fd96415e69a4a77d92a74b04b2dd684c9a938f7
taoxuming <taoxuming@hys-inc.cn>
2017-06-05 16:06:52 +0800

    update

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 import 'isomorphic-fetch';
-
-const a = { ...b };
-
+import React from 'react';
+import ReactDom from 'react-dom';
 
 // global
+const a = { ...b };
+
+ReactDOM.render(
+ <div>sdf</div>,
+ document.getElementById('app')
+);
